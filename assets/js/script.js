eec3144864c8f3b64f00c147357e5ade6f50eea5
anullator <danyelle.joy@gmail.com>
2024-04-02 04:39:15 -0500

    added function body to create a unique id

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,37 +2,43 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
+// ✅ create a function to generate a unique task id
 function generateTaskId() {
-
+    return Math.floor(Math.random()*Date.now()).toString(16);
 }
 
-// Todo: create a function to create a task card
+// TODO: create a function to create a task card
 function createTaskCard(task) {
 
 }
 
-// Todo: create a function to render the task list and make cards draggable
+// TODO: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
 }
 
-// Todo: create a function to handle adding a new task
+// TODO: create a function to handle adding a new task
 function handleAddTask(event){
 
 }
 
-// Todo: create a function to handle deleting a task
+// TODO: create a function to handle deleting a task
 function handleDeleteTask(event){
 
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+// TODO: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// TODO: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
 });
+
+
+//TEST STATEMENTS
+console.log(generateTaskId());
+console.log(generateTaskId());
+console.log(generateTaskId());
