a698e5c67b9e30ca04c3ef3097a6a59643bb5519
anullator <danyelle.joy@gmail.com>
2024-04-02 19:47:47 -0500

    form input stored in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,48 @@ function renderTaskList() {
 // TODO: create a function to handle adding a new task
 function handleAddTask(event){
 
+    event.preventDefault();
+
+    // assign input content to variables
+    const title = $("#title").val().trim();
+    const dueDate = $("#due-date").val().trim(); 
+    const description = $("#description").val().trim();
+
+    let taskList = [];
+
+    // create task object
+    const task = { 
+        title: title,
+        dueDate: dueDate, 
+        description: description, 
+        status: "todo",
+    };
+
+    // checks that all inputs are valid
+    if (!title || !dueDate|| !description) {
+        alert("All inputs are required to add a task.");
+        return;
+    }
+
+    // gets tasks from local storage
+    if (localStorage.length !== 0) {
+        const storage = localStorage.getItem("taskList");
+        taskList = JSON.parse(storage);
+    }
+
+    // add new task to task list
+    taskList.push(task);
+    console.log(taskList);
+
+    // update task list in local storage
+    const stringList = JSON.stringify(taskList);
+    localStorage.setItem("taskList", stringList);
+
+    // empty form input fields
+    $("#title").val("");
+    $("#due-date").val(""); 
+    $("#description").val("");
+
 }
 
 // TODO: create a function to handle deleting a task
@@ -35,10 +77,13 @@ function handleDrop(event, ui) {
 // TODO: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
-   $(".btn-success").on("click", handleAddTask) // add event listener to open modal
+    // const tasks = JSON.parse(localStorage.getItem("taskList")) || [];
+
+    // sort tasks into their respective columns
+
+    $("[type='submit']").on("click", handleAddTask) // add event listener to open modal
 
 });
 
 
 //TEST STATEMENTS
-console.log(generateTaskId());
